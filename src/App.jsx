06c8a85faa6e167bf173b89c61670235e608fb2e
javonjax <javonj461@hotmail.com>
2024-02-22 15:59:21 -0500

    Modified formatting of notes list

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ const App = ({ notes }) => {
       <h1>Notes</h1>
       <ul>
         {notes.map(note =>
-          <li key={note.id}>{note.content}</li>
+          <li key={note.id}>
+            {note.content}
+          </li>
         )}
       </ul>
     </div>

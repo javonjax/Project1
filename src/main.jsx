94453b1e43f762a9af3ecb99f967026164e1cd6a
javonjax <javonj461@hotmail.com>
2024-02-22 15:55:39 -0500

    Initialized notes array and display in a list.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
+const notes = [
+  {
+    id: 1,
+    content: 'HTML',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'JavaScript',
+    important: true
+  },
+  {
+    id: 3,
+    content: 'CSS',
+    important: false
+  }
+]
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App notes={notes}/>
 )
